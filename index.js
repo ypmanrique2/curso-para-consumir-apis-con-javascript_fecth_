9131b5e25a1664ee9333bd9e8827f550f85ffcf9
YADIN PAULO MANRIQUE MARQUEZ <ypmanrique2@misena.edu.co>
2022-11-17 08:17:30 -0500

    consumir-apis-con-js-fecth

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(
-        (response) => response.json()   
+        (response) => {
+            if(response.status >= 200 && response.status < 300){
+
+            }
+            console.log(response);
+            return response.json()
+        }  
     ).then(
         (data) => console.log(data)
+    ).catch(
+        err => console.log(err)
     )
