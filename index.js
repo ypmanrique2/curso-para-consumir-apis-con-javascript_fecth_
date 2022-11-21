82e3cfc658241ac6dedb6fa63d94b8ce71a780c4
YADIN PAULO MANRIQUE MARQUEZ <ypmanrique2@misena.edu.co>
2022-11-21 18:13:56 -0500

    2.3.Metodos

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(
-        (response) => {
+
+let parametros = {
+    userId: 1,
+    _limit: 3
+}
+
+let url = new URL('https://jsonplaceholder.typicode.com/posts');
+
+Object.keys(parametros).forEach(paramKey => {
+    url.searchParams.append(
+         paramKey,
+        parametros[paramKey]
+    )
+});
+
+fetch(url).then(r => r.json()).then(data => console.log(data));
+
+/* fetch()
+    .then(response=>{
             if(response.status >= 200 && response.status < 300){
 
             }
-            console.log(response);
-            return response.json()
-        }  
-    ).then(
+            return response.json();
+    }).then(
         (data) => console.log(data)
-    ).catch(
-        err => console.log(err)
-    )
+    ).catch(err=>{
+        console.log(err);
+    }) */
